f62e544f0ec02a95afba07da59ef9709cc54d72c
Maryna Ushakova <ginger4795@gmail.com>
2022-07-10 18:15:53 +0300

    task 02 updated

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,40 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryListEl = document.querySelector('.gallery');
+
+const makeGalleryItemsEl = ({ preview, original, description } = {}) => {
+  return `
+  <div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>
+  `;
+};
+
+const galleryItemsArray = galleryItems.map(makeGalleryItemsEl);
+
+galleryListEl.insertAdjacentHTML('afterbegin', galleryItemsArray.join(''));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionSelector: 'img',
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+// ****** - так не работает:
+// const onImageClick = event => {
+//   event.preventDefault();
+
+//   if (event.target.nodeName !== 'IMG') {
+//     return;
+//   }
+//   const lightbox = new SimpleLightbox('.gallery a', {
+//     captionSelector: 'img',
+//     captionDelay: 250,
+//     captionsData: 'alt',
+//   });
+// };
+// *********
