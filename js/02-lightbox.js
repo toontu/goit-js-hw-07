4708f20dba3404e661fad444fe6018fdb29b07df
Maryna Ushakova <ginger4795@gmail.com>
2022-09-12 17:55:44 +0300

    task 02 added

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+// Сделай такую же галерею как в первом задании, но используя библиотеку SimpleLightbox, которая возьмет на себя обработку кликов по изображениям, открытие и закрытие модального окна, а также пролистывание изображений при помощи клавиатуры. Посмотри демо видео работы галереи с подключенной библиотекой.
+
+// Необходимо немного изменить разметку карточки галереи, используй этот шаблон.
+
+// <a class="gallery__item" href="large-image.jpg">
+//   <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+// </a>
+// Выполняй это задание в файлах 02-lightbox.html и 02-lightbox.js. Разбей его на несколько подзадач:
+
+// Создание и рендер разметки по массиву данных galleryItems и предоставленному шаблону элемента галереи. Используй готовый код из первого задания.
+// Подключение скрипта и стилей библиотеки используя CDN сервис cdnjs. Необходимо добавить ссылки на два файла: simple-lightbox.min.js и simple-lightbox.min.css.
+// Инициализация библиотеки после того как элементы галереи созданы и добавлены в div.gallery. Для этого ознакомься с документацией SimpleLightbox - в первую очередь секции «Usage» и «Markup».
+// Посмотри в документации секцию «Options» и добавь отображение подписей к изображениям из атрибута alt. Пусть подпись будет снизу и появляется через 250 миллисекунд после открытия изображения.
+
+const imageGalleryEl = document.querySelector('.gallery');
+
+const makeGalleryItem = ({ preview, original, description } = {}) => {
+  return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+};
+
+const galleryItemsArr = galleryItems.map(makeGalleryItem);
+// const galleryItemsArr = galleryItems.map(el => {
+//   return makeGalleryItem(el);
+// });
+
+imageGalleryEl.innerHTML = galleryItemsArr.join('');
+// console.log(galleryItemsArr.join(''));
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+
+// console.dir(lightbox);
+// console.dir(SimpleLightbox);
 console.log(galleryItems);
